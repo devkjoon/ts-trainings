41bedee24c141011ff6700443206d92517fe59e6
devkjoon <devkjoon@gmail.com>
2024-06-03 14:01:22 -0400

    create mongodb server

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+const createProduct = async (req, res, next) => {
+    
+};
+
+const getProducts = async (req, res, next) => {
+
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://127.0.0.1:27017/tsTraining', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connection Success")
+    })
+    .catch(err => {
+        console.log("Connection Failed")
+        console.log(err)
+    })
+    
+const userSchema = new mongoose.Schema({
+    name: String,
+    username: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+const kevin = new User ({ name: 'Kevin Alvarenga', username: 'kevinalvarenga', password: 'y9t69257wv'});

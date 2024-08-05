12627bc6603c10fb759ebe5dadb36000830236e7
devkjoon <devkjoon@gmail.com>
2024-08-05 16:54:46 -0400

    revised admintokenverification hook

--- client/src/hooks/AdminTokenVerification.jsx
+++ client/src/hooks/AdminTokenVerification.jsx
@@ -5,27 +5,43 @@ import API_URL from '../config';
 
 export default function AdminTokenVerification() {
 
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
+      const token = localStorage.getItem('token');
+
+      if(!token){
+        alert('You are not authenticated. Redirecting to homepage');
+        navigate('/');
+        return;
+      }
+
       try {
         const response = await fetch(`${API_URL}/admin/protected-resource`,{
           headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-          }
+            Authorization: `Bearer ${token}`
+          },
         });
+
         if (!response.ok) {
           throw new Error('Failed to fetch protected resource');
         }
+
         const responseData = await response.json();
         setData(responseData);
       } catch (error) {
         console.error('Error fetching data:', error);
-        navigate('/student/dashboard');
+        
+        localStorage.removeItem('token');
+        alert('Authentication failed. Redirecting to homepage');
+        navigate('/');
       }
     };
+
     fetchData();
   }, [navigate]);
+
+  return data;
 }

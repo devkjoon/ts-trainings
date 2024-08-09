7b33945af8a321fbf316c8f12991694d28d2ea9e
devkjoon <devkjoon@gmail.com>
2024-08-09 08:30:39 -0400

    added deployed origin to cors on app.js

--- server/app.js
+++ server/app.js
@@ -16,10 +16,23 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://ts-trainings-2f55296deb14.herokuapp.com'
+];
+
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));

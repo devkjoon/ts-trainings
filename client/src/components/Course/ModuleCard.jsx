5e2cf7310302bca14d17a4851c386717df0a0b8d
devkjoon <devkjoon@gmail.com>
2024-08-19 14:12:28 -0400

    horizontal cards applied to smaller screens on the module dashboard

--- client/src/components/Course/ModuleCard.jsx
+++ client/src/components/Course/ModuleCard.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Card, Button, Col } from 'react-bootstrap';
-
-import '../../assets/css/ModuleCard.css'
+import '../../assets/css/ModuleCard.css';
 
 const ModuleCard = ({ module, flipped, onFlip, onButtonClick, buttonText, buttonVariant }) => (
-  <Col sm={12} md={6} lg={4} className="mb-4 d-flex col-center">
-    <Card className={`module-card h-100 w-100 ${flipped ? 'flipped' : ''}`}>
+  <Col xs={12} md={6} lg={4} className="mb-4 d-flex">
+    <Card className={`module-card ${flipped ? 'flipped' : ''}`}>
       <div className="card-inner">
-        <div className="card-front d-flex" onClick={onFlip}>
-          <Card.Img className="moduleCardImg" src={module.moduleIconUrl || 'default-image-url.jpg'} alt="Module Image" />
-          <Card.Body className="card-body-flex">
-            <Card.Title className="module-content m-auto">{module.title}</Card.Title>
-          </Card.Body>
+        <div className="card-front" onClick={onFlip}>
+          <div className="img-title-container">
+            <div className="img-container">
+              <Card.Img
+                className="module-card-img"
+                src={module.moduleIconUrl || 'default-image-url.jpg'}
+                alt="Module Image"
+              />
+            </div>
+            <Card.Body className="card-body-flex">
+              <Card.Title className="module-content">{module.title}</Card.Title>
+            </Card.Body>
           </div>
-          <div className="card-footer-container">
           <Card.Footer className="card-footer">
             <Button
               className="module-dashboard-btn"
@@ -29,11 +34,11 @@ const ModuleCard = ({ module, flipped, onFlip, onButtonClick, buttonText, button
           </Card.Footer>
         </div>
         <div className="card-back" onClick={onFlip}>
-          <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+          <Card.Body className="card-back-body">
             <div className="description-header">
               <strong>About this Module:</strong>
             </div>
-            <ul className="description-list scrollable-description text-left">
+            <ul className="scrollable-description">
               {module.description.map((desc, index) => (
                 <li key={index}>{desc}</li>
               ))}

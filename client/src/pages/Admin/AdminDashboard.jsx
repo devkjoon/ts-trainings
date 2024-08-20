8abddad7802ae6d0fdc980bf6d9050e8eb2e603c
devkjoon <devkjoon@gmail.com>
2024-08-20 12:19:44 -0400

    mobile support for admin dashboard

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Row, Col, } from 'react-bootstrap';
+import { Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import AdminTokenVerification from '../../hooks/AdminTokenVerification';
@@ -8,25 +8,25 @@ import '../../assets/css/Dashboard.css';
 
 export default function AdminDashboard() {
 
-  AdminTokenVerification()
+  AdminTokenVerification();
 
   return (
     <>
       <div className='dashboardContainer'>
         <Row>
-          <Col className='text-center'>
+          <Col sm={12} lg={4} className='text-center mb-3'>
             <Link to='/admin/students' className='no-underline'>
               <Button className="dashboardBtn" variant="outline-warning" size="lg">Students</Button>
             </Link>
           </Col>
-            <Col className='text-center'>
-            <Link to='/admin/courses' className='no-underline'>
-              <Button className="dashboardBtn" variant="outline-info" size="lg">Course Directory</Button>
+          <Col sm={12} lg={4} className='text-center mb-3'>
+            <Link to="/admin/companies" className='no-underline'>
+              <Button className="dashboardBtn" variant="outline-info" size="lg">Companies</Button>
             </Link>
           </Col>
-          <Col className='text-center'>
-            <Link to="/admin/companies" className='no-underline'>
-              <Button className="dashboardBtn" variant="outline-warning" size="lg">Companies</Button>
+          <Col sm={12} lg={4} className='text-center mb-3'>
+            <Link to='/admin/courses' className='no-underline'>
+              <Button className="dashboardBtn" variant="outline-warning" size="lg">Course Directory</Button>
             </Link>
           </Col>
         </Row>
